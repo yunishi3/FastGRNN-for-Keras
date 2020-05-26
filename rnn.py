37224af3438976333551109accfe2ae01b99061a
yu nishimura <xi.cun.you.327@gmail.com>
2020-05-26 13:54:17 +0900

    add FastRNNcell

--- rnn.py
+++ rnn.py
@@ -159,36 +159,36 @@ class FastGRNNCellKeras(RNNCell):
     def build(self, input_shape):
         input_dim = input_shape[-1]
         if self._wRank is None:
-            W_matrix_init = RandomNormal(0,1)
+            W_matrix_init = RandomNormal(0,0.1)
             self.W = self.add_weight(
                 shape=(input_dim, self._hidden_size),
                 name='W',
                 initializer=W_matrix_init)
         else:
-            W_matrix_1_init = RandomNormal(0,1)
+            W_matrix_1_init = RandomNormal(0,0.1)
             self.W1 = self.add_weight(
                 shape=(input_dim, self._wRank),
                 name='W1',
                 initializer=W_matrix_1_init)
-            W_matrix_2_init = RandomNormal(0,1)
+            W_matrix_2_init = RandomNormal(0,0.1)
             self.W2 = self.add_weight(
                 shape=(self._wRank, self._hidden_size),
                 name='W2',
                 initializer=W_matrix_2_init)
 
         if self._uRank is None:
-            U_matrix_init = RandomNormal(0,1)
+            U_matrix_init = RandomNormal(0,0.1)
             self.U = self.add_weight(
                 shape=(self._hidden_size, self._hidden_size),
                 name='U',
                 initializer=U_matrix_init)
         else:
-            U_matrix_1_init = RandomNormal(0,1)
+            U_matrix_1_init = RandomNormal(0,0.1)
             self.U1 = self.add_weight(
                 shape=(self._hidden_size, self._uRank),
                 name='U1',
                 initializer=U_matrix_1_init)
-            U_matrix_2_init = RandomNormal(0,1)
+            U_matrix_2_init = RandomNormal(0,0.1)
             self.U2 = self.add_weight(
                 shape=(self._uRank, self._hidden_size),
                 name='U2',
@@ -261,6 +261,179 @@ class FastGRNNCellKeras(RNNCell):
         Vars.extend([self.zeta, self.nu])
 
         return Vars
+
+class FastRNNCellKeras(RNNCell):
+    '''
+    FastRNN Cell with Both Full Rank and Low Rank Formulations
+    Has multiple activation functions for the gates
+    hidden_size = # hidden units
+
+    update_non_linearity = nonlinearity for final rnn update
+    can be chosen from [tanh, sigmoid, relu, quantTanh, quantSigm]
+
+    wRank = rank of W matrix (creates two matrices if not None)
+    uRank = rank of U matrix (creates two matrices if not None)
+    alphaInit = init for alpha, the update scalar
+    betaInit = init for beta, the weight for previous state
+
+    FastRNN architecture and compression techniques are found in
+    FastGRNN(LINK) paper
+
+    Basic architecture is like:
+
+    h_t^ = update_nl(Wx_t + Uh_{t-1} + B_h)
+    h_t = sigmoid(beta)*h_{t-1} + sigmoid(alpha)*h_t^
+
+    W and U can further parameterised into low rank version by
+    W = matmul(W_1, W_2) and U = matmul(U_1, U_2)
+    '''
+
+    def __init__(self, hidden_size, update_non_linearity="tanh",
+                 wRank=None, uRank=None, alphaInit=-3.0, betaInit=3.0,
+                 name="FastRNN", reuse=None, **kwargs):
+        self._hidden_size = hidden_size
+        self._update_non_linearity = update_non_linearity
+        self._num_weight_matrices = [1, 1]
+        self._wRank = wRank
+        self._uRank = uRank
+        self._alphaInit = alphaInit
+        self._betaInit = betaInit
+        if wRank is not None:
+            self._num_weight_matrices[0] += 1
+        if uRank is not None:
+            self._num_weight_matrices[1] += 1
+        self._name = name
+        super(FastRNNCellKeras, self).__init__(**kwargs)#_reuse=reuse)
+        #self._reuse = reuse
+
+    @property
+    def state_size(self):
+        return self._hidden_size
+
+    @property
+    def output_size(self):
+        return self._hidden_size
+
+    @property
+    def update_non_linearity(self):
+        return self._update_non_linearity
+
+    @property
+    def wRank(self):
+        return self._wRank
+
+    @property
+    def uRank(self):
+        return self._uRank
+
+    @property
+    def num_weight_matrices(self):
+        return self._num_weight_matrices
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def cellType(self):
+        return "FastRNN"
+
+    def build(self, input_shape):
+        input_dim = input_shape[-1]
+        if self._wRank is None:
+            W_matrix_init = RandomNormal(0,0.1)
+            self.W = self.add_weight(
+                shape=(input_dim, self._hidden_size),
+                name='W',
+                initializer=W_matrix_init)
+        else:
+            W_matrix_1_init = RandomNormal(0,0.1)
+            self.W1 = self.add_weight(
+                shape=(input_dim, self._wRank),
+                name='W1',
+                initializer=W_matrix_1_init)
+            W_matrix_2_init = RandomNormal(0,0.1)
+            self.W2 = self.add_weight(
+                shape=(self._wRank, self._hidden_size),
+                name='W2',
+                initializer=W_matrix_2_init)
+
+        if self._uRank is None:
+            U_matrix_init = RandomNormal(0,0.1)
+            self.U = self.add_weight(
+                shape=(self._hidden_size, self._hidden_size),
+                name='U',
+                initializer=U_matrix_init)
+        else:
+            U_matrix_1_init = RandomNormal(0,0.1)
+            self.U1 = self.add_weight(
+                shape=(self._hidden_size, self._uRan),
+                name='U1',
+                initializer=U_matrix_1_init)
+            U_matrix_2_init = RandomNromal(0,0.1)
+            self.U2 = self.add_weight(
+                shape=(self._uRank, self._hidden_size),
+                name='U2',
+                initializer=U_matrix_2_init)
+
+        alpha_init = Constant(self._alphaInit)
+        self.alpha = self.add_weight(
+            shape=(1,1),
+            name='alpha',
+            initializer=alpha_init)
+
+        beta_init = Constant(self._betaInit)
+        self.beta = self.add_weight(
+            shape=(1,1),
+            name='beta',
+            initializer=beta_init) 
+        bias_update_init = Ones()
+        self.bias_update = self.add_weight(
+            shape=(1, self._hidden_size),
+            name='B_h',
+            initializer=bias_update_init)
+        self.built=True
+
+
+    def call(self, inputs, state, training=None):
+        if self._wRank is None:
+            wComp = K.dot(inputs, self.W)
+        else:
+            wComp = K.dot(
+                K.dot(inputs, self.W1), self.W2)
+
+        if self._uRank is None:
+            uComp = K.dot(state, self.U)
+        else:
+            uComp = K.dot(
+                K.dot(state, self.U1), self.U2)
+
+        pre_comp = wComp + uComp
+
+        c = gen_non_linearity_keras(
+            pre_comp + self.bias_update, self._update_non_linearity)
+
+        new_h = K.sigmoid(self.beta) * \
+            state + K.sigmoid(self.alpha) * c
+        return new_h, new_h
+
+    def getVars(self):
+        Vars = []
+        if self._num_weight_matrices[0] == 1:
+            Vars.append(self.W)
+        else:
+            Vars.extend([self.W1, self.W2])
+
+        if self._num_weight_matrices[1] == 1:
+            Vars.append(self.U)
+        else:
+            Vars.extend([self.U1, self.U2])
+
+        Vars.extend([self.bias_update])
+        Vars.extend([self.alpha, self.beta])
+
+        return Vars
+
 
 class FastGRNNCell(RNNCell):
     '''
